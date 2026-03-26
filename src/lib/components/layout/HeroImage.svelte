287eb1cd6dfa1b1614700218f20a0048fd4871b6
Lukas Schneider <privat.lukas.schneider@gmail.com>
2026-03-26 17:41:17 +0100

    Changes before first presentation

--- src/lib/components/layout/HeroImage.svelte
+++ src/lib/components/layout/HeroImage.svelte
@@ -6,4 +6,4 @@
     
 </script>
 
-<div class="w-full h-[40vh] bg-cover bg-center bg-no-repeat" style="background-image: url({getDirectusAssetURL(image?.id ?? PUBLIC_FALLBACK_IMAGE_ID)});"></div>
+<div class="w-full h-[40vh] bg-cover bg-center bg-no-repeat" style="background-image: url({getDirectusAssetURL(image?.id ?? "04a26447-8ef3-4abe-b7a9-d4c8471ac2f7")});"></div>
